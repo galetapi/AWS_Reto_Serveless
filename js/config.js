9af64397c4c08e1b9001268ef452073fb0c124c1
Daniel Galeano <you@example.com>
2023-05-12 09:28:25 -0500

    App para desplegar en AWS primera version

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_7tpA9rcBS', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '32ldq0c1ht20gdbi7b7k9sq2as', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: 'https://u50es6tyc6.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
